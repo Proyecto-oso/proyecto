37c5d6853aea9838d416fe8504102b17d2cfc4b6
gantiva <dsgantivac@unal.edu.co>
2018-10-08 01:48:35 -0500

    director edit

--- director.php
+++ director.php
@@ -1,3 +1,38 @@
+<style media="screen">
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+.titles {
+  margin-top: 50px;
+
+}
+
+input{
+  float: right;
+}
+
+.creation{
+  float: left;
+}
+
+</style>
+
+
+
 <?php
 include_once("header.php");
 ?>
@@ -8,6 +43,124 @@ include_once("header.php");
     <a href="pruebas.php">Crear prueba</a>
     </div>
 </section>
+
+
 <?php
+
+$arrayName = array();
+
+
+?>
+
+
+<h1 class="titles">Estudiantes</h1>
+<form class="" action="report.php" method="post">
+  <input type="submit" class="creation" name="create_student" value="Crear estudiante">
+</form>
+
+<table>
+  <tr>
+    <th>Nombre</th>
+    <th>Identificacion</th>
+  </tr>
+
+
+
+<?php
+  // set the resulting array to associative
+  $sql = 'SELECT * FROM estudiantes';
+  foreach ($dbh->query($sql) as $row) {
+    echo '
+            <tr>
+              <form class="" action="report.php" method="post">
+                <input type="hidden" name="student" value="'.$row['id'].'">
+                <th>'.$row['nombre'].'</th>
+                <th>'.$row['documento'].'
+                <input type="submit" name="ver estduiante" value="Ver Estudiante"></th>
+              </form>
+            </tr>
+          ';
+
+  }
+
+?>
+</table>
+
+
+<h1 class="titles">Psicologos</h1>
+
+<form class="" action="report.php" method="post">
+  <input type="submit" class="creation" name="create_psicologo" value="Crear psicologo">
+</form>
+
+
+<table>
+
+    <tr>
+      <th><a  > Nombre </a></th>
+      <th>Correo</th>
+    </tr>
+
+<?php
+  // set the resulting array to associative
+  $sql = 'SELECT * FROM psicologos';
+
+  foreach ($dbh->query($sql) as $row) {
+    echo '
+            <tr>
+              <form class="" action="report.php" method="post">
+                <input type="hidden" name="psicologo" value="'.$row['id'].'">
+                <th>'.$row['nombre'].'</th>
+                <th>'.$row['correo'].'
+                <input type="submit" name="ver estduiante" value="Ver psicologo"></th>
+              </form>
+            </tr>
+          ';
+  }
+
+?>
+</table>
+
+<h1 class="titles">co-talleristas</h1>
+
+<form class="" action="report.php" method="post">
+  <input type="submit" class="creation" name="create_co-tallerista" value="Crear co-tallerista">
+</form>
+
+<table>
+  <tr>
+    <th>Nombre</th>
+    <th>Correo</th>
+  </tr>
+
+<?php
+  // set the resulting array to associative
+  $sql = 'SELECT * FROM `co-talleristas`';
+
+  foreach ($dbh->query($sql) as $row) {
+
+    echo '
+            <tr>
+              <form class="" action="report.php" method="post">
+                <input type="hidden" name="co_tallerista" value="'.$row['id'].'">
+                <th>'.$row['nombre'].'</th>
+                <th>'.$row['correo'].'
+                <input type="submit" name="ver estduiante" value="Ver co-tallerista"></th>
+              </form>
+            </tr>
+          ';
+
+
+
+  }
+
+?>
+</table>
+
+
+
+<?php
+
 include_once("footer.php");
+
 ?>
--- report.php
+++ report.php
@@ -2,6 +2,74 @@
 
   include_once("header.php");
 
+
+
+  if( isset($_POST['student']) )
+  {
+    $sql = 'SELECT * FROM estudiantes WHERE id = '.$_POST['student'].'';
+    foreach ($dbh->query($sql) as $row) {
+        print "ID = ".$row['id'] . "<br/>";
+        print "Nombre = ".$row['nombre'] . "<br/>";
+        print "Documento =".$row['documento'] . "<br/>";
+    }
+  }
+  elseif( isset($_POST['psicologo']) )
+  {
+    $sql = 'SELECT * FROM psicologos WHERE id = '.$_POST['psicologo'].'';
+    foreach ($dbh->query($sql) as $row) {
+        print "ID = ".$row['id'] . "<br/>";
+        print "Nombre = ".$row['nombre'] . "<br/>";
+        print "Correo =".$row['correo'] . "<br/>";
+    }
+  }
+
+
+  elseif( isset($_POST['co_tallerista']) )
+  {
+    $sql = 'SELECT * FROM `co-talleristas` WHERE id = '.$_POST['co_tallerista'].'';
+    foreach ($dbh->query($sql) as $row) {
+        print "ID = ".$row['id'] . "<br/>";
+        print "Nombre = ".$row['nombre'] . "<br/>";
+        print "Correo =".$row['correo'] . "<br/>";
+    }
+  }
+
+  elseif( isset($_POST['create_student']) )
+  {
+    ?>
+    <h1>Crear estudiante</h1>
+    <form class="" action="index.html" method="post">
+      Nombre: <input type="text" name="" value="">
+      Documento: <input type="text" name="" value="">
+    </form>
+
+    <?php
+  }
+
+  elseif( isset($_POST['create_psicologo']) )
+  {
+    ?>
+    <h1>Crear Psicologo</h1>
+    <form class="" action="index.html" method="post">
+      Nombre: <input type="text" name="" value="">
+      Correo: <input type="text" name="" value="">
+    </form>
+
+    <?php
+  }
+
+  elseif( isset($_POST['create_co-tallerista']) )
+  {
+    ?>
+    <h1>Crear Co-tallerista</h1>
+    <form class="" action="index.html" method="post">
+      Nombre: <input type="text" name="" value="">
+      Correo: <input type="text" name="" value="">
+    </form>
+
+    <?php
+  }
+  /*
   $sql = 'SELECT * FROM estudiantes JOIN  grupos';
   foreach ($dbh->query($sql) as $row) {
       print_r($row) ;
@@ -35,5 +103,6 @@
 
 <?php
 
+*/
 
 ?>
