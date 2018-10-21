8dc8689b0d246cbb32c8f3757364d2fa8b742a98
Diego Duarte <dduartec@unal.edu.co>
2018-10-21 11:32:32 -0500

    vista directores s3 y s4

--- director.php
+++ director.php
@@ -258,14 +258,16 @@ function showGrupos($dbh)
   }
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $_SESSION['grupo_id'] = $_POST['id'];
-  echo '<h3>Institucion: ' . $_POST['name'] . '</h3>';
-  echo '<a href="sesion1.php"><h5>SESIÓN 1</h5></a>';
-  echo '<a href="sesion2.php"><h5>SESIÓN 2</h5></a>';
-} else {
-  echo '<h3>Instituciones:</h3>';
-  showGrupos($dbh);
-}
+        $_SESSION['grupo_id'] = $_POST['id'];
+        echo '<h3>Institucion: ' . $_POST['name'] . '</h3>';
+        echo '<a href="sesion1.php"><h5>SESIÓN 1</h5></a>';
+        echo '<a href="sesion2.php"><h5>SESIÓN 2</h5></a>';
+        echo '<a href="sesion3.php"><h5>SESIÓN 3</h5></a>';
+        echo '<a href="sesion4.php"><h5>SESIÓN 4</h5></a>';
+    } else {
+        echo '<h3>Instituciones:</h3>';
+        showGrupos($dbh);
+    }
 ?>
 <?php
 include_once("footer.php");
